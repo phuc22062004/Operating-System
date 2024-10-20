c8df7e60cfc73eb451506dd441a957a1e026af49
phuc22062004 <anhphucx22@gmail.com>
2024-10-20 12:26:09 +0700

    Adjust pingpong and primes

--- user/primes.c
+++ user/primes.c
@@ -1,70 +1,81 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-void primes() {
-    int prime_pipe[2];
-    int prime_number;
+void primes(int left_pipe)
+{
+    int prime;
 
-    if (read(0, (void*)&prime_number, sizeof(prime_number)) <= 0)
-        return;
-
-    printf("prime %d\n", prime_number);
-
-    if (pipe(prime_pipe) < 0) {
-        fprintf(2, "Error: cannot create pipe\n");
-        exit(1);
-    }
-
-    int pid = fork(); 
-    if (pid == 0) {
-        close(0);               
-        dup(prime_pipe[0]);    
-        close(prime_pipe[0]);   
-        close(prime_pipe[1]);   
-        primes();               
-    } else {
-        close(1);               
-        dup(prime_pipe[1]);     
-        close(prime_pipe[0]);  
-        close(prime_pipe[1]);  
-
-        int next_number = 0;
-        while (read(0, (void*)&next_number, sizeof(next_number)) > 0) {
-            if (next_number % prime_number != 0) {
-                write(1, (void*)&next_number, sizeof(next_number));  
-            }
+    while (1) {
+        if (read(left_pipe, &prime, sizeof(prime)) == 0) {
+            close(left_pipe);
+            exit(0); 
         }
-        close(1); 
-        wait(&pid);  
+
+        printf("prime %d\n", prime);
+
+        int num;
+        int right_pipe[2];
+
+        if (pipe(right_pipe) < 0) {
+            fprintf(2, "Error: cannot create pipe\n");
+            exit(1);
+        }
+
+        int pid = fork();
+        if (pid < 0) {
+            fprintf(2, "Error: fork failed\n");
+            exit(1);
+        } else if (pid == 0) {
+            close(left_pipe);          
+            close(right_pipe[1]);      
+            left_pipe = right_pipe[0]; 
+        } else {
+            close(right_pipe[0]);      
+
+            while (read(left_pipe, &num, sizeof(num)) > 0) {
+                if (num % prime != 0) {
+                    write(right_pipe[1], &num, sizeof(num));  
+                }
+            }
+
+            close(left_pipe);
+            close(right_pipe[1]);
+
+            wait(0);
+            exit(0);
+        }
     }
 }
 
-int main(void) {
-    int main_pipe[2]; 
-    if (pipe(main_pipe) < 0) {
-        fprintf(2, "Error: cannot create pipe\n");
+int main(int argc, char *argv[])
+{
+    int initial_pipe[2];
+
+    if (pipe(initial_pipe) < 0) {
+        fprintf(2, "Error: cannot create initial pipe\n");
         exit(1);
     }
 
-    int pid = fork();  
-    if (pid == 0) {
-        close(0);              
-        dup(main_pipe[0]);    
-        close(main_pipe[0]);   
-        close(main_pipe[1]);  
-        primes();              
+    int pid = fork();
+    if (pid < 0) {
+        fprintf(2, "Error: fork failed\n");
+        exit(1);
+    } else if (pid == 0) {
+        close(initial_pipe[1]);    
+        primes(initial_pipe[0]);  
     } else {
-        close(1);            
-        dup(main_pipe[1]);     
-        close(main_pipe[0]);   
-        close(main_pipe[1]);   
+        close(initial_pipe[0]);   
 
+        
         for (int i = 2; i <= 280; i++) {
-            write(1, (void*)&i, sizeof(i));
+            write(initial_pipe[1], &i, sizeof(i));
         }
 
-        close(1);  
-        wait(&pid); 
+        
+        close(initial_pipe[1]);
+
+        
+        wait(0);
+        exit(0);
     }
-    exit(0);
 }
